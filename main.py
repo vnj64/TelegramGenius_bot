92888a63ebd9500340e2aa6834fd3f9b8b5e46a6
Kamil Khabibullin <vnj644@gmail.com>
2022-09-29 11:13:00 +0300

    All functions with RAPIDAPI

--- main.py
+++ main.py
@@ -1,42 +1,9 @@
-import jsonschema
+import json
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
-DRIVER = webdriver.Chrome(executable_path="/home/sirius/TelegramGenius/chromedriver/chromedriver")
-
-def genius_parse(url):
-
-    artist_name = 'OG BUDA'
-
-    if artist_name:
-        try:
-            DRIVER.get(url=url)
-            
-            time.sleep(3)
-            
-            search = DRIVER.find_element('xpath', '//*[@id="application"]/div/div[1]/div[1]/form/input')
-            search.click()
-            search.send_keys(f'{artist_name}')
-            
-            search.send_keys(Keys.ENTER)
-
-            time.sleep(6)
-
-            # first_artist = DRIVER.find_element('xpath', '/html/body/div[1]/search-form/form/div[2]/div/search-results/div[2]/div/search-result-section[1]/div/div[2]/search-result-items/div/search-result-item/div/mini-artist-card/a/div[2]/div')
-
-            # first_artist.click()
-        
-
-        except Exception as ex:
-            print(ex)
-
-        finally:
-            DRIVER.close()
-            DRIVER.quit()
-    else:
-        return "Введите имя артиста: "
+from config import *
 
 
 
@@ -80,33 +47,66 @@ def songs_milano():
 def top_chart():
     url = "https://genius-song-lyrics1.p.rapidapi.com/songs/chart"
 
-    querystring = {"time_period":"day","chart_genre":"all","per_page":"50","page":"1"}
+    querystring = {"time_period":"day","chart_genre":"all","per_page":"10","page":"1"}
 
     headers = {
-        "X-RapidAPI-Key": "d8a2b24394msh530522f623e5ebcp1dff13jsn3401538088fc",
+        "X-RapidAPI-Key": "c30a3e4eefmsh005fbb18a21016ap15e87cjsnac664dd22942",
         "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
 
-    jsonData = response.json()
-    print(jsonData)
+    print(response.text)
 
-def artists_chart()
-	url = "https://genius-song-lyrics1.p.rapidapi.com/artists/chart"
 
-	querystring = {"time_period":"day","per_page":"10","page":"2"}
+def new_func():
+    url = "https://genius-song-lyrics1.p.rapidapi.com/search"
 
-	headers = {
-		"X-RapidAPI-Key": "d8a2b24394msh530522f623e5ebcp1dff13jsn3401538088fc",
-		"X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
-	}
+    querystring = {"q":"Scally Milano","per_page":"10","page":"1"}
 
-	response = requests.request("GET", url, headers=headers, params=querystring)
+    headers = {
+        "X-RapidAPI-Key": "c30a3e4eefmsh005fbb18a21016ap15e87cjsnac664dd22942",
+        "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
+    }
 
-	jsonData = response.json()['response']
-	for data in jsonData['chart_items']:
-		print(data['item']['name'])
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
-	for url in jsonData['chart_items']:
-		print(data['item']['url'])
+    print(response.text)
+
+
+def artists_chart():
+
+    url = "https://genius-song-lyrics1.p.rapidapi.com/artists/chart"
+
+    querystring = {"time_period":"day","per_page":"10","page":"1"}
+
+    headers = {
+        "X-RapidAPI-Key": "c30a3e4eefmsh005fbb18a21016ap15e87cjsnac664dd22942",
+        "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    jsonData = response.json()['response']
+	
+    for data in jsonData['chart_items']:
+        print(data['item']['name'])
+
+
+def take_lyrics():
+    url = "https://genius-song-lyrics1.p.rapidapi.com/songs/6329375/lyrics"
+
+    headers = {
+        "X-RapidAPI-Key": "c30a3e4eefmsh005fbb18a21016ap15e87cjsnac664dd22942",
+        "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    result = []
+    
+    jsonData = response.json()['response']
+    double = jsonData['lyrics']
+    bd = double
+    result = bd['lyrics']['body']
+    print(result['html'])
